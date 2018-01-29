1a2ad46ef33cd203eaee54ea2ce0b96df44d5a8f
Agustin Sarasua <agustin.sarasua@mercadolibre.com>
2018-01-29 13:27:35 -0300

    fix

--- app/property_validation.go
+++ app/property_validation.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"time"
 
 	c "github.com/agustin-sarasua/rs-common"
@@ -12,19 +11,19 @@ func validateProperty(p *m.Property) []error {
 	var errs []error
 
 	//errs = c.ValidateExistInMap(m.PropertyTypes, p.Type, "Type is incorrect", errs)
-	errs = c.ValidateExistInMap(m.Orientation, p.Orientation, "Orientation is incorrect", errs)
-	errs = c.ValidateRangeCondition(0, 10, p.State, fmt.Sprintf("State should be between %v and %v", 0, 10), errs)
-	errs = c.ValidateCondition(func() bool { return p.Address != nil }, "Address can not be empty", errs)
+	//errs = c.ValidateExistInMap(m.Orientation, p.Orientation, "Orientation is incorrect", errs)
+	//errs = c.ValidateRangeCondition(0, 10, p.State, fmt.Sprintf("State should be between %v and %v", 0, 10), errs)
+	//errs = c.ValidateCondition(func() bool { return p.Address != nil }, "Address can not be empty", errs)
 	errs = c.ValidatePositiveNumber(p.Bedrooms, "Bedrooms should be positive", errs)
 	errs = c.ValidatePositiveNumber(p.Bathrooms, "Bathrooms should be positive", errs)
-	errs = c.ValidatePositiveNumber(p.Kitchens, "Kitchens should be positive", errs)
+	//errs = c.ValidatePositiveNumber(p.Kitchens, "Kitchens should be positive", errs)
 	errs = c.ValidateRangeCondition(1800, time.Now().Year(), p.ConstructionYear, "ConstructionYear should be between 1800 and Now", errs)
-	errs = c.ValidatePositiveNumber(p.Elevators, "Elevators should be positive", errs)
-	errs = c.ValidatePositiveNumber(p.CourtyardSize, "CourtyardSize should be positive", errs)
+	//errs = c.ValidatePositiveNumber(p.Elevators, "Elevators should be positive", errs)
+	//errs = c.ValidatePositiveNumber(p.CourtyardSize, "CourtyardSize should be positive", errs)
 	errs = c.ValidatePositiveNumber(p.Size, "Size should be positive", errs)
 	errs = c.ValidatePositiveNumber(p.Floors, "Floors should be positive", errs)
-	errs = c.ValidatePositiveNumber(p.GarageSize, "GarageSize should be positive", errs)
+	errs = c.ValidatePositiveNumber(p.Garages, "Garages should be positive", errs)
 	errs = c.ValidatePositiveNumber(p.ConstructionSize, "ConstructionSize should be positive", errs)
-	errs = c.ValidatePositiveNumber(p.Showers, "Showers should be positive", errs)
+	//errs = c.ValidatePositiveNumber(p.Showers, "Showers should be positive", errs)
 	return errs
 }
